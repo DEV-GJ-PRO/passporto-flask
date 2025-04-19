7f2f675f41b4d032d0a4f53f81c5f66afcb22889
Dev GJ Pro <devgjpro@gmail.com>
2025-04-19 17:44:29 +0530

    port

--- app.py
+++ app.py
@@ -141,4 +141,5 @@ def privacy():
     return render_template('privacy.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))  # default fallback port
+    app.run(host="0.0.0.0", port=port)
